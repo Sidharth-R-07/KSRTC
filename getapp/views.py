bc95d4e07c186adef2d0c2e2adb1c8c87eb8dd55
Sidharth-R-07 <sidhuram365@gmail.com>
2024-02-27 11:20:18 +0530

    .

--- getapp/views.py
+++ getapp/views.py
@@ -1,27 +1,23 @@
-from django.contrib.auth.models import User, Group
+# views.py
 
+import requests
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-from rest_framework import permissions, viewsets
+@api_view(['GET'])
+def get_bus_details(request):
+    # URL for GTFS realtime data
+    url_gtfs_realtime = 'http://external.chalo.com/dashboard/gtfs/realtime/thiruvananthapuram/ksrtc/bus'
 
-from getapp.serializers import UserSerializer, GroupSerializer
+    # Headers
+    headers = {
+        'externalauth': 'RWLXTEgMcmuMj1mehBWi3ROaAfTmQwXjGksxvxD9'
+    }
 
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]  
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    
-
+    # Make GET request for GTFS realtime data
+    response = requests.get(url_gtfs_realtime, headers=headers)
+    if response.status_code == 200:
+        gtfs_realtime_data = response.json()  # Assuming response is JSON
+        return Response(gtfs_realtime_data)
+    else:
+        return Response({'error': 'Failed to fetch GTFS realtime data'}, status=500)
