36955f8dcea49120cc4ff249ab907baa1d04f2b4
Sidharth-R-07 <sidhuram365@gmail.com>
2024-02-29 14:58:57 +0530

    .

--- server/views.py
+++ server/views.py
@@ -5,7 +5,6 @@ import logging
 import os
 from dotenv import load_dotenv
 
-
 from tenacity import retry, stop_after_attempt, wait_exponential
 
 # Configure a proper logging setup using a library like logging
@@ -15,7 +14,6 @@ load_dotenv()
 
 @api_view(['GET'])
 def get_bus_details(request):
-    logger.info("***********************Received request for GTFS realtime data*************************")
     """
     Fetches GTFS realtime bus data from the external API and returns it as a response.
 
@@ -26,7 +24,8 @@ def get_bus_details(request):
         Response object containing GTFS realtime data (on success) or an error message (on failure).
     """
 
-    url_gtfs_realtime = 'http://external.chalo.com/dashboard/gtfs/realtime/thiruvananthapuram/ksrtc/bus'
+    url_gtfs_realtime = 'https://external.chalo.com/dashboard/gtfs/realtime/thiruvananthapuram/ksrtc/bus'
+    print("***********************Received request for GTFS realtime data*************************")
 
     # Read authentication token from a secure source (e.g., environment variable)
     try:
@@ -45,7 +44,7 @@ def get_bus_details(request):
         retry=requests.exceptions.RequestException  # Retry on any request exceptions
     )
     def fetch_gtfs_data():
-        response = requests.get(url_gtfs_realtime, headers=headers, timeout=10)
+        response = requests.get(url_gtfs_realtime, headers=headers, timeout=15)
         response.raise_for_status()  # Raise an exception for non-200 status codes
         return response.json()
 
@@ -56,4 +55,6 @@ def get_bus_details(request):
     except requests.exceptions.RequestException as e:
         logger.error(f"Failed to fetch GTFS realtime data: {e}")
         return Response({'error': 'Failed to fetch data'}, status=500)
-
+    except ValueError as ve:
+        logger.error(f"Failed to parse response as JSON: {ve}")
+        return Response({'error': 'Invalid JSON response'}, status=500)
