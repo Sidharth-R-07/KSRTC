117b271814849ee9cd64f6e2f7d3ae45c5c0d7ef
Sidharth-R-07 <sidhuram365@gmail.com>
2024-02-29 23:11:38 +0530

    ,

--- server/views.py
+++ server/views.py
@@ -1,21 +1,22 @@
-import requests
+from django.http import JsonResponse
 from rest_framework.decorators import api_view
-
+import requests
 
 @api_view(['GET'])
 def get_bus_details(request):
-
     url_gtfs_realtime = 'http://external.chalo.com/dashboard/enterprise/v1/vehicle/sessionData/thiruvananthapuram/ksrtc?vehicleId=KS3132'
-    print("***********************Received request for GTFS realtime data*************************")
     headers = {
         'externalauth': 'RWLXTEgMcmuMj1mehBWi3ROaAfTmQwXjGksxvxD9'
     }
     try:
         response = requests.get(url_gtfs_realtime, headers=headers)
-        print('Responsde Content:', response.content)
-        print('Response Status:', response.status_code)
-        print('Response Headers:', response.headers)
-        print('Response Text:', response.text)
-        print('Response JSON:', response.request)
-    except Exception as e: 
-        print("*******************An error occurred: {e}*************************")
+        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+        data = response.json()
+        print(f'Data:', data)
+        # Process data as needed
+        return JsonResponse(data)  # Return the fetched data as a JSON response
+    except Exception as e:
+        # Log the error
+        print(f"An error occurred: {e}")
+        # Return an appropriate error response
+        return JsonResponse({'error': 'An error occurred while fetching bus details'}, status=500)
